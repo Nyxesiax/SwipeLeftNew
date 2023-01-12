24fb9a5587fbbdc790a9b4485d889ec4cf1f0005
Mark Fischer <mark.fischer0404@gmail.com>
2023-01-12 14:12:20 +0100

    enum

--- app/src/main/java/com/example/swipeleft/Videos.java
+++ app/src/main/java/com/example/swipeleft/Videos.java
@@ -1,5 +1,12 @@
 package com.example.swipeleft;
 
+import android.provider.MediaStore;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
 public enum Videos {
     A("IdneKLhsWOQ"),
     B(""),
@@ -22,6 +29,15 @@ public enum Videos {
 
     Videos(String videoId){
         this.videoId = videoId;
+    }
+
+    private static final List<Videos> VALUES =
+            Collections.unmodifiableList(Arrays.asList(values()));
+    private static final int SIZE = VALUES.size();
+    private static final Random RANDOM = new Random();
+
+    public static Videos randomLetter()  {
+        return VALUES.get(RANDOM.nextInt(SIZE));
     }
 
 }
